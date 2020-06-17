0d55fa8a832a8b27b9a73c8a5aa4291378a3004a
Rahul Bhardwaj ツ <51912472+Rayzon3@users.noreply.github.com>
2020-06-17 13:49:28 +0530

    Add files via upload

---
+++ encript.c
@@ -0,0 +1,46 @@
+#include<stdio.h> 
+#include<string.h> 
+#include<ctype.h>
+#define MAX_LIMIT 100
+
+int encript();
+
+
+int main()
+{
+    return encript();
+}
+
+
+int encript()
+{
+    char input[MAX_LIMIT];
+    char store[MAX_LIMIT];
+    int i;
+    int key;
+    
+    
+    printf("Enter the text to encript: \n");
+    fgets(input, MAX_LIMIT, stdin);
+    printf("Enter the key: \n");
+    scanf("%d", &key);
+
+    for(i = 0; i < strlen(input); i++)
+    {
+        store[i] = input[i] + key;
+        
+    }
+
+    printf("The encripted text is: \n");
+    for(i = 0; i < strlen(input); i++)
+    {
+        printf("%c", store[i]);
+    }
+
+    printf("\n");
+
+
+    return 0;
+    
+}
+
